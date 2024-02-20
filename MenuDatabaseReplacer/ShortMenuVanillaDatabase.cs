4f2956cafc60cf989720a3b5eec88deacd4cdfb8
krypto5863 <krypto5863@users.noreply.github.com>
2024-02-19 21:37:30 -0500

    Ver bump

--- MenuDatabaseReplacer/ShortMenuVanillaDatabase.cs
+++ MenuDatabaseReplacer/ShortMenuVanillaDatabase.cs
@@ -16,7 +16,7 @@ using System.Security.Permissions;
 namespace ShortMenuVanillaDatabase
 {
 	//This is the metadata set for your plugin.
-	[BepInPlugin("ShortMenuVanillaDatabase", "ShortMenuVanillaDatabase", "1.3.1")]
+	[BepInPlugin("ShortMenuVanillaDatabase", "ShortMenuVanillaDatabase", "1.3.2")]
 	[SuppressMessage("ReSharper", "InconsistentNaming")]
 	[SuppressMessage("ReSharper", "RedundantAssignment")]
 	public class ShortMenuVanillaDatabase : BaseUnityPlugin
